cfec113d710e4bb5fb0437c305b0c205355d9e71
mrjlancaster <lancast001@gmail.com>
2020-10-23 18:43:46 -0500

    add some styles and event listener to add

---
+++ main.js
@@ -0,0 +1,21 @@
+let input = document.querySelector('.input');
+const addBtn = document.querySelector('.addBtn');
+const ul = document.querySelector('.ul');
+
+const todos = [];
+
+let list = '';
+
+addBtn.addEventListener('click', () => {
+    // store input value in variable
+    let newItem = input.value;
+
+    // add input value to an li
+    list += `<li>${newItem}</li>`;
+
+    // add li with input value to ul
+    ul.innerHTML = list;
+
+    // clear input field
+    input.value = '';
+})
