c160bf2ea2e1223b123fa9ed5a61cd432f051ae9
mrjlancaster <lancast001@gmail.com>
2020-10-24 12:41:26 -0500

    add trash button

--- main.js
+++ main.js
@@ -1,33 +1,48 @@
 let input = document.querySelector('.input');
 const addBtn = document.querySelector('.addBtn');
 const ul = document.querySelector('.ul');
+const deleteAllBtn = document.querySelector('.clear_all-btn')
 
 const todos = [];
 
 let list = '';
 
 addBtn.addEventListener('click', () => {
+    // if input value is empty, do nothing
+    if (input.value === '') {
+        return false;
+    }
+
     // store input value in variable
     let newItem = input.value;
 
     // add input value to an li
-    list += `<li class="item">${newItem}</li>`;
+    list += `
+    <li class="item">${newItem}
+    <button class="trashBtn"><i class="far fa-trash-alt"></i></button>
+    </li>`
+   ;
+
 
     // add li with input value to ul
     ul.innerHTML = list;
 
     // clear input field
     input.value = '';
-})
 
+   
+})
 
 
 // DELETE FUNCTION
 ul.addEventListener('click', (e) => {
-    console.log('ul working');
-    if (e.target.matches('li')) {
-        e.target.style.textDecoration = 'line-through';
+    const item = e.target;
+    if (item.matches('li')) {
+        item.style.textDecoration = 'line-through';
     }
 })
 
-
+// CLEAR ALL
+deleteAllBtn.addEventListener('click', () => {
+    console.log('clear all button is working');
+})
