e34932ed237ab30cd73d496ca6c2c190b323b6b2
Jonathan Lancaster <lancast001@gmail.com>
2023-02-12 15:32:49 -0600

    code refactor

--- main.js
+++ main.js
@@ -1,109 +1,114 @@
-let input = document.querySelector('.input');
-const addBtn = document.querySelector('.addBtn');
-const ul = document.querySelector('.ul');
-const deleteAllBtn = document.querySelector('.clear_all-btn');
-let todoList = [];
+let input = document.querySelector(".input");
+const addBtn = document.querySelector(".addBtn");
+const ul = document.querySelector(".ul");
+const deleteAllBtn = document.querySelector(".clear_all-btn");
+let myTodos = [];
 
-// ADD BUTTON EVENT HANDLER
-addBtn.addEventListener('click', () => {
-    // If input value is empty, do nothing
-    const task = input.value;
-    if (task === '') {
-        return false;
+function getTodos() {
+  const list = JSON.parse(localStorage.getItem("todos"));
+
+  if (list == null) {
+    return;
+  } else {
+    myTodos = list;
+  }
+}
+
+function setTodos(list) {
+  myTodos = list;
+  localStorage.setItem("todos", JSON.stringify(list));
+}
+
+function addTodo() {
+  addBtn.addEventListener("click", () => {
+    const inputValue = input.value;
+
+    if (!inputValue) {
+      alert("A value must be entered in the input field");
+      return;
     }
 
-    addItem(task);
-})
+    const task = { id: myTodos.length + 1, task: inputValue };
+    myTodos.push(task);
+    localStorage.setItem("todos", JSON.stringify(myTodos));
+    input.value = "";
+  });
+}
 
-
-// ADD NEW ITEM TO TODO LIST AND LOCAL STORAGE
-const addItem = (item) => {
-    // add item to array (list)
-    todoList.push(item);
-    addDataToLocalStorage(todoList);
-
-    // clear input field
-    input.value = '';
+function deleteItem(id) {
+  const newTodo = myTodos.filter((item) => item.id !== Number(id));
+  setTodos(newTodo);
 }
 
 // HANDLING TODO ITEMS DISPLAY
 const displayTodos = (todos) => {
-    ul.innerHTML = '';
+  todos.forEach((item) => {
+    // create li element to add item
+    const li = document.createElement("li");
+    li.setAttribute("id", item.id);
 
-    todos.forEach(item => {
-        // create li element to add item
-        const li = document.createElement('li');
+    // create class for li element
+    li.classList.add("item");
 
-        // create class for li element
-        li.classList.add('item');
+    // create action buttons
+    const buttons = `<span><button class="editBtn"><i class="far fa-edit"></i></button>
+        <button class="trashBtn"><i id=${item.id} class="far fa-trash-alt"></button></span>`;
 
-        // create action buttons
-        const buttons = `<span><button class="editBtn"><i class="far fa-edit"></i></button>
-        <button class="trashBtn"><i class="far fa-trash-alt"></button></span>`
+    li.innerHTML = `<span id=${item.id} class="item-text" contenteditable="false">${item.task}</span> ${buttons}`;
+    firstItem = ul.firstChild;
+    ul.insertBefore(li, firstItem);
+  });
+};
 
-        li.innerHTML = `<span class="item-text" contenteditable="false">${item}</span> ${buttons}`;
-        firstItem = ul.firstChild;
-        ul.insertBefore(li, firstItem);
-    })
-}
-
-
-// EDITING ITEMS FUNTION
-ul.addEventListener('click', (e) => {
+// Handle editing and deletings tasks
+function handleEditAndDeleteItem() {
+  ul.addEventListener("click", (e) => {
     const item = e.target;
 
-    if (item.matches('i')) {
-        // Delete item
-        if (item.className === 'far fa-trash-alt') {
-            const parentEl = e.target.parentNode;
-            const spanEl = parentEl.parentNode;
-            spanEl.parentNode.remove();
-        }
+    if (item.matches("i")) {
+      // Delete item
+      if (item.className === "far fa-trash-alt") {
+        deleteItem(item.id);
+        // const parentEl = e.target.parentNode;
+        // const spanEl = parentEl.parentNode;
+        // spanEl.parentNode.remove();
+      }
 
-        // Edit item
-        if (item.className === 'far fa-edit') {
-            const spanTag = document.getElementsByTagName('span');
-            const editableText = spanTag[0];
-            editableText.contentEditable = true;
-            editableText.classList.add('editable-text');
-            editableText.focus();
+      // Edit item
+      if (item.className === "far fa-edit") {
+        const spanTag = document.getElementsByTagName("span");
+        const editableText = spanTag[0];
+        editableText.contentEditable = true;
+        editableText.classList.add("editable-text");
+        editableText.focus();
 
-            editableText.addEventListener('keydown', (e) => {
-                if (e.key === 'Enter') {
-                    editableText.contentEditable = false;
-                    editableText.classList.remove('editable-text');
-                }
-            })    
-        }
+        editableText.addEventListener("keydown", (e) => {
+          if (e.key === "Enter") {
+            editableText.contentEditable = false;
+            editableText.classList.remove("editable-text");
+          }
+        });
+      }
     }
-})
-
-// ADDING DATA TO LOCAL STORAGE
-const addDataToLocalStorage = (todos) => {
-    localStorage.setItem('todos', JSON.stringify(todos));
-    displayTodos(todoList);
+  });
 }
 
-// GET TODO LIST FROM LOCAL STORAGE FOR DISPLAY
-const getDataFromLocalStorage = () => {
-    const tasks = localStorage.getItem('todos');
-    todos = JSON.parse(tasks);
-    displayTodos(todoList);
+function clearList() {
+  deleteAllBtn.addEventListener("click", () => {
+    if (myTodos.length) {
+      localStorage.clear();
+      myTodos = [];
+
+      const list = document.querySelectorAll(".ul li");
+      for (let i = 0; (li = list[i]); i++) {
+        li.parentNode.removeChild(li);
+      }
+    }
+  });
 }
 
-// CLEAR ALL
-deleteAllBtn.addEventListener('click', () => {
-    // clear local storage
-    localStorage.clear();
-
-    // clear todos
-    todoList = [];
-
-    const list = document.querySelectorAll('.ul li');
-    for (let i = 0; li = list[i]; i++) {
-        li.parentNode.removeChild(li)
-    }
-})
-
-
-getDataFromLocalStorage();
+getTodos();
+addTodo();
+displayTodos(myTodos);
+handleEditAndDeleteItem();
+clearList();
